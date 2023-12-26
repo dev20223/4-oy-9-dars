0163cb3cde277ecb41dde1e4e49d3cada844749a
dev20223 <chingizbekturdiboyev@gmail.com>
2023-12-26 16:39:06 +0500

    Vazifa  Bajarildi

---
+++ main.js
@@ -0,0 +1,77 @@
+// Vazifa Bajarildi !!!
+
+// 1-masala
+// function volueArray(arr, value) {
+//     for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === value) {
+//     return "Qiymat mavjud :)";
+//     }
+//     }
+//     return "Qiymat mavjud emas :(";
+//     }
+    
+//     let array = [1, 3, 5, 7, 9];
+//     let volArr = 2;
+    
+//     console.log(volueArray(array, volArr));
+
+// 2-masala
+// const products = [
+//     { id: 1, name: 'Laptop', brand: 'Dell', price: 999.99 },
+//     { id: 2, name: 'Smartphone', brand: 'Samsung', price: 699.99 },
+//     { id: 3, name: 'Tablet', brand: 'Apple', price: 499.99 },
+//     { id: 4, name: 'Desktop', brand: 'HP', price: 1199.99 },
+//     ];
+    
+// function filterProducts(products, id, value) {
+//     return products.filter(product => product[id] === value);
+//     }
+
+//     console.log(filterProducts(products, 'brand', 'Apple'));
+   
+
+
+// 3-masala
+// const students = [
+//     { name: 'Alice', age: 22, grades: [85, 90, 92, 88] },
+//     { name: 'Bob', age: 20, grades: [78, 85, 80, 88] },
+//     { name: 'Charlie', age: 21, grades: [90, 92, 89, 94] },
+//     ];
+// function ageArray(students) {
+   
+//     let sum = 0;
+//     for (let i = 0; i < students.length; i++) {
+//     sum += students[i].age;
+//     }
+//     return sum / students.length;
+//     }
+    
+//     console.log(ageArray(students));
+
+//   4-masala
+// const taskArray = (tasks) => {
+//     tasks.sort((a, b) => {
+//     return new Date(a.deadline) - new Date(b.deadline);
+//     });
+//     }
+    
+//     const tasks = [
+//     { taskName: 'Task A', priority: 'High', deadline: '2023-03-15' },
+//     { taskName: 'Task B', priority: 'Medium', deadline: '2023-04-10' },
+//     { taskName: 'Task C', priority: 'Low', deadline: '2023-03-01' },
+//     { taskName: 'Task D', priority: 'High', deadline: '2023-03-20' },
+//     ];
+    
+//     taskArray(tasks);
+    
+//     console.log(tasks);
+
+// 5-masala
+// function sliceArray(arr, start, end) {
+//     return arr.slice(start, end);
+//   }
+//   let arr = [1, 2, 3, 4, 5, 6, 7];
+//   let result = sliceArray(arr, 2, 5);
+//   console.log(result); 
+    
+
